360e879f93caa08b91d7e9adec5afbbd4f72c7d3
鮫島泰史 <h.sameshima@wiz-net.jp>
2024-09-23 22:17:00 +0900

    修正

--- index.js
+++ index.js
@@ -5,25 +5,33 @@ const accessToken = process.env.LINE_NOTIFY_ACCESS_TOKEN;
 
 
 export const handler = async (event) => {
-
-    try {
-        const message = JSON.parse(event.body);
-        // const message = 'test lambda 111111';
-
-        const postMsg = await axios.post(
-            url,
-            {
-                message
-            },
-            {headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Authorization': 'Bearer ' + accessToken
-                }});
-    } catch (error) {
-        console.error('Error:', error);
-    } finally {
-
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(event),
+  };
+  try {
+    const body = JSON.parse(event.body);
+    const message = body.message;
+    if (!message) {
+      return response;
     }
+    await axios.post(
+      url,
+      {
+        message
+      }, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Bearer ' + accessToken
+        }
+      });
+  } catch (error) {
+    console.error('Error:', error);
+  } finally {
+
+  }
+
+  return response;
 };
 
 await handler();
